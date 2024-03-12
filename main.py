c7bd2b00a32582c9d9de2e3f1524f0440e81d997
Marsboy02 <marsboy0619@gmail.com>
2024-03-12 14:48:35 +0900

    feat: 파일 업로드 api 구현

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import os
+
 from flask import Flask, jsonify, request
 from util.tts import text_to_speech
 
 app = Flask(__name__)
 
+# mp4 파일이 업로드되는 경로 설정
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def hello_world():
@@ -20,6 +25,27 @@ def post_tts_request():
     result = text_to_speech(text)
 
     return jsonify({'result': result})
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+
+    file = request.files['file']
+
+    # 사용자가 파일을 선택하지 않고 form을 제출했을 경우
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+
+    # 파일이 있는 경우
+    if file:
+        # 안전한 파일 이름을 생성 (secure_filename 사용 권장)
+        filename = file.filename
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+
+        return jsonify({'message': f'File {filename} uploaded successfully'}), 200
 
 
 if __name__ == '__main__':
