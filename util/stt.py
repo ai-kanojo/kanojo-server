71ba54d2de908d16d0f17650ee14fa5e48ca855c
Hyeongjun Kang <86903678+marsboy02@users.noreply.github.com>
2024-03-13 16:31:57 +0900

    feat: speech_recognition 모듈을 통한 stt 추가 (#3)

---
+++ util/stt.py
@@ -0,0 +1,15 @@
+import speech_recognition as sr
+
+
+def speech_to_text(audio_file_path):
+    recognizer = sr.Recognizer()
+
+    try:
+        with sr.AudioFile(audio_file_path) as source:
+            audio_data = recognizer.record(source)
+            text = recognizer.recognize_google(audio_data)
+            return text
+    except sr.UnknownValueError:
+        return "Google Speech Recognition could not understand audio"
+    except sr.RequestError as e:
+        return f"Could not request results from Google Speech Recognition service; {e}"
